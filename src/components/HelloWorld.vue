5ab8f000804d64c891f7dee020d6d1d769ac16ba
Abdul Rauf <editsabdulrauf5681@gmail.com>
2024-07-10 11:15:48 -0700

    fc

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="() => {
+  if(count > 10){
+    count = 0;
+  } else {
+    count++;
+  }}">count is {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
